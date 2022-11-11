2dd484fe78d3dffa5830bcd2828195fcfbaf1e4b
surya <psschand16@gamil.com>
2022-11-11 13:59:35 +0000

    docker

--- indock.sh
+++ indock.sh
@@ -54,7 +54,11 @@ echo "########################################################"
 
 # echo "portainer.................."
 
-# docker run -d  -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+# sudo docker run -d  -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+
+# sudo docker run -d  -p 127.0.0.1:9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+
+#  0.0.0.0:9000->9000/tcp
 echo "#######################snaps#################################"
 export PASSWORD=omgam
 echo $PASSWORD
